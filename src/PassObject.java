b5c98bd116a8ecdede893b236c8084da91c91c91
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-09-05 22:54:53 +0300

    error

---
+++ src/PassObject.java
@@ -0,0 +1,25 @@
+
+public class PassObject {
+	static void f(Calculator y);{
+		y.c = 124.f;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator x = new Calcular();
+		x.c = 364.f;
+		System.out.println("1: x.c: " + x.c);
+		f(x);
+		System.out.println("2: x.c: " + x.c);
+	}
+
+}
+
+
+
+
+
+	
+	// TODO Auto-generated method stub
+
+
