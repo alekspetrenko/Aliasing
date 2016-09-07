d5c99f3476cfd22863ccadf54ffa3c31151c4ee9
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-09-07 22:56:21 +0300

    вызов метода

--- src/PassObject.java
+++ src/PassObject.java
@@ -1,21 +1,21 @@
 
 public class PassObject {
+	PassObject b = new PassObject();
 	public void f(Calculator y) {
 		y.c = 124.f;
 		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Calculator x = new Calcular();
+		
+		Calculator x = new Calculator();
 		x.c = 364.f;
 		System.out.println("1: x.c: " + x.c);
-		f(x);
+		b.f(x);
 		System.out.println("2: x.c: " + x.c);
 	}
 
 }
-
-
 
 
 
