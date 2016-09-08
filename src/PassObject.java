d9dc386e77aa021ad50ac252c957e1dccd00263e
Aleksandr Petrenko <petrenko.aleksandr025@gmail.com>
2016-09-08 19:54:36 +0300

    methodd

--- src/PassObject.java
+++ src/PassObject.java
@@ -1,13 +1,12 @@
 
 public class PassObject {
-	PassObject b = new PassObject();
-	public static void f(Calculator y) {
+	public void f(Calculator y) {
 		y.c = 124.f;
 		}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		PassObject b = new PassObject();
 		Calculator x = new Calculator();
 		x.c = 364.f;
 		System.out.println("1: x.c: " + x.c);
